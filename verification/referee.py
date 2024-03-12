32107f622a1ca6461bcf29815bed2ec8e3feb4ce
rafaabbade <82163300+rafaabbade@users.noreply.github.com>
2024-03-12 15:21:25 -0300

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,7 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "minimo_viagens",
+            "python": "entrega",
             "js": "countConsecutiveSummers"
         },
         cover_code={
